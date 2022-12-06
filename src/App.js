23a33b36608f957ff671fcef9b81a0c99d174261
Shekhar Deuba <shekhardeuba36@gmail.com>
2022-12-06 14:09:16 +0545

    first project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Button from "./Button";
+const App = () => {
+    return <div>
+         <h1>Welcome to Useful Website </h1>
+         <h3> Which website do you want to open ?</h3>
+         <Button name ="Google" link = "https://google.com"/>
+         <Button name ="Facebook" link = "https://facebook.com"/>
+         <Button name ="Youtube" link = "https://youtube.com" />
+         <Button name ="Instagram" link = "https://instagram.com"/>
+         <br></br>
+         <Button name ="IPO result" link = "https://www.sharesansar.com/ipo-result"/>
+         <Button name ="Github" link = "https://github.com"/>
+         <Button name ="Twitter" link = "https://twitter.com"/>
+         
     </div>
-  );
 }
-
-export default App;
+export default App
